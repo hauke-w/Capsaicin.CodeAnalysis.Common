f3daddd47331f841f3cfd7a1ce726a334cb6102d
Hauke Wittern <hauke.wittern@minebea-intec.com>
2021-03-28 13:07:21 +0200

    added test

---
+++ Capsaicin.CodeAnalysis.Common.Tests/Extensions/TypeSymbolExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capsaicin.CodeAnalysis.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Microsoft.CodeAnalysis;
+
+namespace Capsaicin.CodeAnalysis.Extensions
+{
+    [TestClass]
+    public class TypeSymbolExtensionsTests
+    {
+        [TestMethod]
+        [DataRow(NullableAnnotation.None, false)]
+        [DataRow(NullableAnnotation.NotAnnotated, false)]
+        [DataRow(NullableAnnotation.Annotated, true)]
+        public void AllowsNullTest(NullableAnnotation nullableAnnotation, bool expected)
+        {
+            var typeSymbolMock = new Mock<ITypeSymbol>(MockBehavior.Strict);
+            typeSymbolMock.SetupGet(it => it.NullableAnnotation).Returns(nullableAnnotation);
+            var actual = TypeSymbolExtensions.AllowsNull(typeSymbolMock.Object);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
