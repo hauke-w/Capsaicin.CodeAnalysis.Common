49141398478b79f6496a68dd51ae7688bccd1e86
Hauke Wittern <hauke.wittern@minebea-intec.com>
2021-04-01 00:26:06 +0200

    added StringBuilderExtensions

---
+++ Capsaicin.CodeAnalysis.Common.Tests/Generators/StringBuilderExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capsaicin.CodeAnalysis.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capsaicin.CodeAnalysis.Generators
+{
+    [TestClass]
+    public class StringBuilderExtensionsTests
+    {
+        [TestMethod]
+        [DataRow("a, b, C", ", ", "a", "b", "C")]
+        [DataRow("Hello test!", " ", "Hello", "test!")]
+        [DataRow("Just one value", ".", "Just one value")]
+        [DataRow("123", "", "1", "2", "3")]
+        [DataRow("", "x")]
+        [DataRow(" A--b--c - d ", "-", " A", "", "b", null, "c ", " d ")]
+        public void AppendJoinTest(string expected, string separator, params string?[]? values)
+        {
+            var stringBuilder = new StringBuilder();
+            StringBuilderExtensions.AppendJoin(stringBuilder, separator, values);
+            var actual = stringBuilder.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AppendJoin_Null_Test()
+        {
+            var stringBuilder = new StringBuilder();
+            StringBuilderExtensions.AppendJoin(stringBuilder, " - ", null);
+            var actual = stringBuilder.ToString();
+            Assert.AreEqual(string.Empty, actual);
+        }
+
+        [TestMethod]
+        [DataRow("", "   ", null, true)]
+        [DataRow("", "   ", null, false)]
+        [DataRow("   ", "   ", "", true)]
+        [DataRow("", " x ", "", false)]
+        [DataRow(" line1\r\n\r\n line3", " ", "line1\n\nline3", false)]
+        [DataRow("-line1\r\n-\r\n-line3", "-", "line1\n\r\nline3", true)]
+        [DataRow("    line1", "    ", "line1", true)]
+        [DataRow("_\r\n_line1", "_", "\nline1", true)]
+        [DataRow("\nline1", null, "\nline1", true)]
+        [DataRow("\nline1", "", "\nline1", true)]
+        public void AppendIndentedTest(string expected, string? indention, string? value, bool indentEmptyLines)
+        {
+            var stringBuilder = new StringBuilder();
+            StringBuilderExtensions.AppendIndented(stringBuilder, indention, value, indentEmptyLines);
+            var actual = stringBuilder.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Capsaicin.CodeAnalysis.Common/Generators/StringBuilderExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Capsaicin.CodeAnalysis.Generators
+{
+    /// <summary>
+    /// Extension methods for <see cref="StringBuilder"/>.
+    /// </summary>
+    public static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Appends all <paramref name="values"/> with <paramref name="separator"/> between each value.
+        /// </summary>
+        /// <param name="stringBuilder">The StringBuilder to which <paramref name="values"/> will be appended.</param>
+        /// <param name="separator">The string that is appended to <paramref name="stringBuilder"/> between each value in <paramref name="values"/>.</param>
+        /// <param name="values">The values to be appended. If null, nothing will be appended.</param>
+        public static void AppendJoin(this StringBuilder stringBuilder, string separator, params string?[]? values)
+        {
+            if (values is { Length: >= 1 })
+            {
+                stringBuilder.Append(values[0]);
+                for (int i = 1; i < values.Length; i++)
+                {
+                    stringBuilder.Append(separator);
+                    stringBuilder.Append(values[i]);
+                }
+            }
+        }
+
+        private static readonly Regex LineBreakRegex = new Regex(@"\r?\n");
+        /// <summary>
+        /// Appends a string to <paramref name="stringBuilder"/> with the specified <paramref name="indention"/>.
+        /// If the <paramref name="indention"/> is null, no indention will be applied.
+        /// If value is null, nothing will be appended to <paramref name="stringBuilder"/>.
+        /// </summary>
+        /// <param name="stringBuilder"></param>
+        /// <param name="indention"></param>
+        /// <param name="value"></param>
+        /// <param name="indentEmptyLines">Specifies whether empty lines must be indented (true) or not (false).</param>
+        public static void AppendIndented(this StringBuilder stringBuilder, string? indention, string? value, bool indentEmptyLines = false)
+        {
+            if (value is not null)
+            {
+                if (indention is not { Length: > 0 })
+                {
+                    stringBuilder.Append(value);
+                }
+                else if (value.Length > 0)
+                {
+                    var lines = LineBreakRegex.Split(value);
+                    string line;
+                    int nLinesWithBreak = lines.Length - 1;
+                    for (int i = 0; i < nLinesWithBreak; i++)
+                    {
+                        line = lines[i];
+                        if (line.Length > 0 || indentEmptyLines)
+                        {
+                            stringBuilder.Append(indention);
+                        }
+                        stringBuilder.AppendLine(line);
+                    }
+
+                    line = lines[nLinesWithBreak];
+                    if (line.Length > 0 || indentEmptyLines)
+                    {
+                        stringBuilder.Append(indention);
+                    }
+                    stringBuilder.Append(line);
+                }
+                else if(indentEmptyLines)
+                {
+                    stringBuilder.Append(indention);
+                }
+            }
+        }
+    }
+}
