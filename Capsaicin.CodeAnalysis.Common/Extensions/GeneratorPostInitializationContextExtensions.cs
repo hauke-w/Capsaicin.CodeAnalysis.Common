7bbfd51a930411a9c0eda3afd3eb6b1cdcf4e32e
Hauke Wittern <hauke.wittern@minebea-intec.com>
2021-03-31 13:13:55 +0200

    added GeneratorPostInitializationContextExtensions

---
+++ Capsaicin.CodeAnalysis.Common/Extensions/GeneratorPostInitializationContextExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Capsaicin.CodeAnalysis.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="GeneratorPostInitializationContext"/>.
+    /// </summary>
+    public static class GeneratorPostInitializationContextExtensions
+    {
+        /// <summary>
+        /// Copies an embedded resource to the output of a source generator
+        /// </summary>
+        /// <param name="context">The context to which the source text is added.</param>
+        /// <param name="sourceGenerator">The source generator from which the assembly containing the embedded resource is derived (both must be in the same assembly).</param>
+        /// <param name="sourceFromResourceSpecifier">Specifies the input resource name and the hint name of the output source text.</param>
+        public static void GenerateSourceFromResource(this GeneratorPostInitializationContext context, ISourceGenerator sourceGenerator, SourceFromResourceSpecifier sourceFromResourceSpecifier)
+        {
+            var resourceAssembly = sourceGenerator.GetType().Assembly;
+            GenerateSourceFromResource(context, resourceAssembly, sourceFromResourceSpecifier);
+        }
+
+        /// <summary>
+        /// Copies an embedded resource to the output of a source generator
+        /// </summary>
+        /// <param name="context">The context to which the source text is added.</param>
+        /// <param name="resourceAssembly">The assembly that contains the embedded resource.</param>
+        /// <param name="sourceFromResourceSpecifier">Specifies the input resource name and the hint name of the output source text.</param>
+        public static void GenerateSourceFromResource(this GeneratorPostInitializationContext context, Assembly resourceAssembly, SourceFromResourceSpecifier sourceFromResourceSpecifier)
+        {
+            var (resourceName, hintName) = sourceFromResourceSpecifier;
+            using var resourceStream = resourceAssembly.GetManifestResourceStream(resourceName);
+            using var resourceStreamReader = new StreamReader(resourceStream);
+            var sourceCode = resourceStreamReader.ReadToEnd();
+            context.AddSource(hintName, SourceText.From(sourceCode, Encoding.UTF8));
+        }
+    }
+}
---
+++ Capsaicin.CodeAnalysis.Common/Extensions/SourceFromResourceSpecifier.cs
@@ -0,0 +1,47 @@
+﻿namespace Capsaicin.CodeAnalysis.Extensions
+{
+    /// <summary>
+    /// Data structure that specifies a <see cref="ResourceName"/> and <see cref="SourceHintName"/> pair.
+    /// <see cref="ResourceName"/> specifies the a resource that will be copied to a source generators output.
+    /// <see cref="SourceHintName"/> is an identifier that can be used to reference the generated source text and must be unique within the generator.
+    /// </summary>
+    public record SourceFromResourceSpecifier(string ResourceName, string SourceHintName)
+    {
+        /// <summary>
+        /// Creates a <see cref="SourceFromResourceSpecifier"/> where the <see cref="ResourceName"/> is the concatenation
+        /// of <paramref name="resourcePrefix"/> and <paramref name="resourceSuffixAndHintName"/>. <see cref="SourceHintName"/> is set to <paramref name="resourceSuffixAndHintName"/>.
+        /// </summary>
+        /// <param name="resourcePrefix">A prefix of the resource id.</param>
+        /// <param name="resourceSuffixAndHintName">Suffix of the resource id and will be used as <see cref="SourceHintName"/>.</param>
+        /// <returns></returns>
+        public static SourceFromResourceSpecifier CreateFromResourcePrefixAndSuffix(string resourcePrefix, string resourceSuffixAndHintName)
+            => new SourceFromResourceSpecifier(resourcePrefix + resourceSuffixAndHintName, resourceSuffixAndHintName);
+
+        /// <summary>
+        /// Creates a <see cref="SourceFromResourceSpecifier"/> where <see cref="ResourceName"/> and <see cref="SourceHintName"/>
+        /// equal the <paramref name="resourceAndHintName"/> parameter.
+        /// </summary>
+        /// <param name="resourceAndHintName"></param>
+        /// <returns></returns>
+        public static SourceFromResourceSpecifier Create(string resourceAndHintName)
+            => new SourceFromResourceSpecifier(resourceAndHintName, resourceAndHintName);
+
+        /// <summary>
+        /// Creates a <see cref="SourceFromResourceSpecifier"/> where <see cref="ResourceName"/> is set to the <paramref name="resourceName"/> parameter
+        /// and <see cref="SourceHintName"/> is set to the <paramref name="resourceName"/> parameter.
+        /// </summary>
+        /// <param name="resourceName"></param>
+        /// <param name="sourceHintName"></param>
+        /// <returns></returns>
+        public static SourceFromResourceSpecifier Create(string resourceName, string sourceHintName)
+            => new SourceFromResourceSpecifier(resourceName, sourceHintName);
+
+        /// <summary>
+        /// Creates a <see cref="SourceFromResourceSpecifier"/> where <see cref="ResourceName"/> and <see cref="SourceHintName"/>
+        /// equal the <paramref name="resourceAndHintName"/> parameter.
+        /// </summary>
+        /// <param name="resourceAndHintName"></param>
+        public static implicit operator SourceFromResourceSpecifier(string resourceAndHintName)
+            => new SourceFromResourceSpecifier(resourceAndHintName, resourceAndHintName);
+    }
+}
